443a877222ec5eb7fd62ce7e40fa3d0a2cecb1be
ggerardy <liftchampion@gmail.com>
2019-01-17 19:07:16 +0300

    Fix: libft/t_vec.h - added t_string struct and functions + NORM

--- libft/t_vec.h
+++ libft/t_vec.h
@@ -63,14 +63,14 @@ typedef struct		s_string
 	size_t		capacity;
 }					t_string;
 
-void                    ft_print_string(t_string *str);
+void				ft_print_string(t_string *str);
 
-void					ft_free_string(t_string **str);
+void				ft_free_string(t_string **str);
 
-int						ft_string_fit(t_string **str);
+int					ft_string_fit(t_string **str);
 
-int						ft_string_push_back(t_string **str, char c);
+int					ft_string_push_back(t_string **str, char c);
 
-t_string				*ft_make_string(size_t init_size);
+t_string			*ft_make_string(size_t init_size);
 
 #endif
