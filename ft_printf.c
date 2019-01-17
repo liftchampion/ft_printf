ec123f738639f65ff64a5a8f90174b646937b4b5
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-17 18:14:17 +0300

    Fix ft_printf: add main logic

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 22:36:59 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/16 23:43:56 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/17 18:13:10 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,17 @@
 
 int	printf(const char *frmt, ...)
 {
-	va_list vl;
+	va_list		vl;
+	t_string	*str;
 
 	va_start(vl, frmt);
+	str = ft_make_string(1);
+	while (*frmt)
+	{
+		ft_find_cntrl(&frmt, &str);
+		ft_printf_parser(&frmt, &vl, vl);
+		ft_stringify(&str, vl);
+	}
 	va_end(vl);
 	return (0);
 }
