0760168e11075eba0d477d4e9ac4654ee8de7459
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-18 15:00:47 +0300

    Fix ft_find_cntrl: add handling of string without contol symbols. Return code for this case is 3

--- ft_find_cntrl.c
+++ ft_find_cntrl.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 14:03:08 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/18 14:54:29 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/18 14:58:58 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int ft_find_cntrl(const char **frmt, t_string **str)
 {
-	while (**frmt && **frmt != '%' && **frmt != '{')
+	while (**frmt != '%' && **frmt != '{')
 	{
 		if (!ft_string_push_back(str, **frmt))
 			return (0);
+		if (!**frmt)
+			return (3);
 		(*frmt)++;
 	}
 	(*frmt)++;
