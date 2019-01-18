1e8e55f32f9a2789a44acca8c10e8104cbc1902a
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-18 15:32:59 +0300

    Fix ft_find_cntrl: return char instead of int. Returned char is either control symbol or '\0' or -1 in case of allocation error

--- ft_find_cntrl.c
+++ ft_find_cntrl.c
@@ -6,25 +6,20 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 14:03:08 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/18 14:58:58 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/18 15:30:52 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_find_cntrl(const char **frmt, t_string **str)
+char	ft_find_cntrl(const char **frmt, t_string **str)
 {
 	while (**frmt != '%' && **frmt != '{')
 	{
 		if (!ft_string_push_back(str, **frmt))
-			return (0);
-		if (!**frmt)
-			return (3);
+			return ((char) -1);
 		(*frmt)++;
 	}
 	(*frmt)++;
-	if (*(*frmt - 1) == '%')
-		return (1);
-	else
-		return (2);
+		return (*(*frmt - 1));
 }
