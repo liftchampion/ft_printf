eb24e8da25f2e251057209ebcb9de07dd3b1971c
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-18 14:46:07 +0300

    Fix ft_printf.c: add ft_ to printf function name; comment out unexisted yet functions; add pritnfing and freeing of t_string structure; wrap ft_find_cntrl in safe if.

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,13 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/16 22:36:59 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/17 18:13:10 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/18 14:39:38 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	printf(const char *frmt, ...)
+int	ft_printf(const char *frmt, ...)
 {
 	va_list		vl;
 	t_string	*str;
@@ -21,10 +21,13 @@ int	printf(const char *frmt, ...)
 	str = ft_make_string(1);
 	while (*frmt)
 	{
-		ft_find_cntrl(&frmt, &str);
-		ft_printf_parser(&frmt, &vl, vl);
-		ft_stringify(&str, vl);
+		if (!ft_find_cntrl(&frmt, &str))
+			return (1);
+		//ft_printf_parser(&frmt, &vl, vl);
+		//ft_stringify(&str, vl);
 	}
+	ft_print_string(str);
+	ft_free_string(&str);
 	va_end(vl);
 	return (0);
 }
