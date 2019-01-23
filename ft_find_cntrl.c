ade40f94376aa52d15feb3881d6d659ccf237c8a
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-24 00:07:20 +0300

    Fix ft_find_cntrl: add check for \; replace push_back function to new, corresponding to changes

--- ft_find_cntrl.c
+++ ft_find_cntrl.c
@@ -6,7 +6,7 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/18 14:03:08 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/23 13:46:41 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/23 19:41:15 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,9 @@ int	ft_find_cntrl(const char **frmt, t_string **str)
 {
 	if (!frmt || !*frmt || !str || !*str)
 		return (0);
-	while (**frmt != '%' && **frmt != '{')
+	while (**frmt != '%' && (**frmt != '{' && *(*frmt - 1) != '\\'))
 	{
-		if (!ft_string_push_back(str, **frmt))
+		if (!ft_string_push_back_c(str, **frmt))
 			return (0);
 		(*frmt)++;
 	}
