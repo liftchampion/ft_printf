2b0b76ad1942397d85065819a2d23f3223629023
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-01-24 00:09:06 +0300

    Fix ft_string_push_back.c : add function to string handle. Fix t_vec: correspond changes

--- libft/ft_string_push_back.c
+++ libft/ft_string_push_back.c
@@ -6,13 +6,13 @@
 /*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/23 15:11:01 by ehugh-be          #+#    #+#             */
-/*   Updated: 2019/01/23 15:11:34 by ehugh-be         ###   ########.fr       */
+/*   Updated: 2019/01/23 15:43:59 by ehugh-be         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int				ft_string_push_back(t_string **str_ptr, char c)
+int	ft_string_push_back_c(t_string **str_ptr, char c)
 {
 	t_string *str;
 
@@ -31,5 +31,19 @@ int				ft_string_push_back(t_string **str_ptr, char c)
 	}
 	str->data[str->len++] = c;
 	str->data[str->len] = 0;
+	return (1);
+}
+
+int	ft_string_push_back(t_string **str_ptr, char *str)
+{
+	int t;
+
+	while (*str)
+	{
+		if ((t = ft_string_push_back_c(str_ptr, *str)) == 1)
+			str++;
+		else
+			return (t);
+	}
 	return (1);
 }
