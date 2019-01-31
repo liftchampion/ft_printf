bea7d76be0474835937b9ebd38bb24d85a297db9
ggerardy <liftchampion@gmail.com>
2019-01-31 18:13:32 +0300

    Fix: ft_printf.h declarations from ft_printf_compose.h moved to ft_printf.h
    Took 50 seconds

--- ft_printf.h
+++ ft_printf.h
@@ -63,5 +63,7 @@ void			*ft_get_va(int num, va_list vl, t_string *a_s);
 void			ft_gen_compose(t_arg_data *v, void *num, t_string **str);
 void			ft_float_compose(t_arg_data *v, void *num, t_string **str);
 int				ft_printf_compose(t_arg_data *arg_data, void *arg,
-												t_string **str, char type);
+		t_string **str, char type);
+int				ft_printf_int_compose(t_arg_data *arg_data, void* arg,
+		t_string **str);
 #endif
