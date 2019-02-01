fb94660faa9023a2d714b2ae08f28ac3af16b517
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-02-02 02:31:23 +0300

    Add ft_printf_float_compose: composing float into string

---
+++ ft_printf_float_compose.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf_float_compose.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ehugh-be <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/31 18:42:46 by ehugh-be          #+#    #+#             */
+/*   Updated: 2019/01/31 18:43:58 by ehugh-be         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+#include <stdio.h>
+#include <math.h>
+#include "ft_printf_parser.h"
+
+int ft_find_whole_size(long double flt, double *dec)
+{
+	int ret;
+
+	*dec = .1;
+	ret = 0;
+	while (flt > 1.0)
+	{
+		ret++;
+		*dec *= 10.0;
+		flt /= 10.0;
+	}
+	return (ret);
+}
+
+int ft_push_whole(long double *flt, double dec, t_string **str)
+{
+	while (dec >= 1.0)
+	{
+		ft_string_push_back(str, (int)(*flt / dec) + '0');
+		*flt -= dec * (int)(*flt / dec);
+		dec /= 10.0;
+	}
+}
+
+double	ft_simp_pow(double n, double *dec, int p)
+{
+	double ret;
+
+	ret = n;
+	while (--p)
+	{
+		ret *= n;
+		*dec *= 10.0;
+	}
+	return (ret);
+}
+
+int ft_printf_float_compose(t_arg_data *arg_d, void *arg, t_string **str)
+{
+	long double flt;
+	double dec;
+	int log;
+
+	if (arg_d->size == DEFAULT)
+		flt = *(double *)arg;
+	else
+		flt = *(long double *) arg;
+	log = ft_find_whole_size(flt, &dec);
+	printf("log = %d", 180, log);
+
+	ft_push_whole(&flt, dec, str);
+	dec = 1.0;
+	ft_string_push_back(str, '.');
+	flt *= ft_simp_pow((double)10, &dec,
+			arg_d->prcsn > 0 ? arg_d->prcsn : DEFAULT_F_PRECISION);
+	flt += flt - (int)flt > .5 ? 1 : 0;
+	ft_push_whole(&flt, dec, str);
+	printf("%Lf\n", flt);
+	flt *= pow(10, 6);
+}
