59c132870e50aa77a39837c25d0cdee581ace2bc
Aleksey Mitskevich <aleks.mitskevich@gmail.com>
2019-02-02 08:10:25 +0300

    Fix ft_printf_float_compose: whole bunch of fixes additions and so on, but e flag works and nothing else is broken, at first sight at least))) I'm too sleepy for smart comments, so let it be as is AZAZAZAZAZAZAzzzzzzzzzzzzzzzzzzzz don't forget to push commit..........

--- ft_printf_float_compose.c
+++ ft_printf_float_compose.c
@@ -15,6 +15,12 @@
 #include <math.h>
 #include "ft_printf_parser.h"
 
+//TODO refactor to reduce number of functions or move some of them to another file
+
+//TODO check for safety!!!
+
+//TODO rounding of when precision is 0
+
 int ft_find_whole_size(long double flt, double *dec)
 {
 	int ret;
@@ -37,58 +43,81 @@ int ft_push_whole(long double *flt, double dec, t_string **str, int sep)
 	{
 		if (sep > 0 && sep-- % 3 == 0)
 			ft_string_push_back(str, FT_SEPARATOR);
-		ft_string_push_back(str, (int)(*flt / dec) + '0');
+		ft_string_push_back(str, (char)((int)(*flt / dec) + '0'));
 		*flt -= dec * (int)(*flt / dec);
 		dec /= 10.0;
 	}
 }
 
-double	ft_simp_pow(double n, double *dec, int p)
+double	ft_push_frctn(long double *flt, double *dec, int p, t_string **str)
 {
-	double ret;
-
-	ret = n;
-	while (--p)
+	if (!p)
+		return (0);
+	ft_string_push_back(str, '.');
+	*dec = .1;
+	while (p--)
 	{
-		ret *= n;
+		*flt *= 10;
 		*dec *= 10.0;
 	}
+	*flt += *flt - (long long)*flt > .5 ? 1 : 0;
+	ft_push_whole(flt, *dec, str, -1);
+}
+
+int	ft_enot_s(long double *flt, t_string **str, double *dec)
+{
+	int ret;
+
+	if (*flt != *flt)
+		*dec = ft_string_push_back_s(str, "nan") - 10000000;
+	if (isinf(*flt))
+		*dec = ft_string_push_back_s(str, "inf") - 10000000;
+	*dec = 1.0;
+	ret = 0;
+	if (*flt >= 10)
+		while (*flt > 10 && ++ret)
+			*flt /= 10;
+	else
+		while (*flt < 1 && --ret)
+			*flt *= 10;
 	return (ret);
 }
+
+void ft_enot_e(t_string **str, int lg, char e)
+{
+	ft_string_push_back(str, e);
+	ft_string_push_back(str, lg > 0 ? '+' : '-');
+	if (FT_ABS(lg) >= 100)
+		ft_string_push_back(str, FT_ABS(lg) / 100 + '0');
+	ft_string_push_back(str, (FT_ABS(lg) > 10) ? FT_ABS(lg) / 10 + '0' : '0');
+	ft_string_push_back(str, lg % 10 + '0');
+}
+
 /*TODO ? solution differs from original printf on big number of digits because of
  * dividing by big value. Suppose that original one gets digits from the end but
  * not form the beggining of number as ours does. Should we fix this differ?*/
-int ft_printf_float_compose(t_arg_data *arg_d, void *arg, t_string **str)
+int ft_printf_float_compose(t_arg_data *a_d, void *arg, t_string **str)
 {
 	long double flt;
 	double dec;
-	int log;
+	int lg;
 	int pad;
 
-	if (arg_d->size == DEFAULT)
-		flt = *(double *)arg;
-	else
-		flt = *(long double *) arg;
-	if (flt < 0)
-	{
-		ft_string_push_back(str, '-');
-		flt *= -1;
-	}
-	log = ft_find_whole_size(flt, &dec);
-
-	pad = arg_d->width - log - arg_d->prcsn - (arg_d->sign == '+') - log / 3;
-	if (arg_d->sign)
-		ft_string_push_back(str, arg_d->sign);
-	if (arg_d->width && !arg_d->l_a)
-		ft_string_push_back_n_c(str, pad, arg_d->ac);
-	ft_push_whole(&flt, dec, str, arg_d->spl ? log : -1);
-	dec = 1.0;
-	ft_string_push_back(str, '.');
-	flt *= ft_simp_pow((double)10, &dec,
-			arg_d->prcsn > 0 ? arg_d->prcsn : DEFAULT_F_PRECISION);
-	flt += flt - (long long)flt > .5 ? 1 : 0;
-	ft_push_whole(&flt, dec, str, -1);
-	if (arg_d->l_a)
-		ft_string_push_back_n_c(str, arg_d->width - log - 1 - arg_d->prcsn, ' ');
-	flt *= pow(10, 6);
+	flt = a_d->size == DEFAULT ? *(double *)arg : *(long double *) arg;
+	flt < 0 ? ft_string_push_back(str, '-') : 0;
+	flt *= flt < 0 ? -1 : 1;
+	lg = ft_find_whole_size(flt, &dec);
+	if (a_d->sign)
+		ft_string_push_back(str, a_d->sign);
+	pad = ft_tolower(a_d->format) == 'e' ? a_d->width - lg - a_d->prcsn -
+			 (a_d->sign == '+') - lg / 3 : a_d->width - 6 - a_d->prcsn;
+	if (a_d->width && !a_d->l_a)
+		ft_string_push_back_n_c(str, pad, a_d->ac);
+	lg = ft_tolower(a_d->format) == 'e' ? ft_enot_s(&flt, str, &dec) : lg;
+	ft_push_whole(&flt, dec, str, a_d->spl ? lg : -1);
+	ft_push_frctn(&flt, &dec, a_d->prcsn >= 0 ? a_d->prcsn : DEF_F_PRCSN, str);
+	if (ft_tolower(a_d->format) == 'e')
+		ft_enot_e(str, lg, a_d->format);
+	if (a_d->l_a)
+		ft_string_push_back_n_c(str, a_d->width - lg - 1 - a_d->prcsn, ' ');
 }
